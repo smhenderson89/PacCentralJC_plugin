f80e6e30610a6692459f583d6855c14cea49f121
Scott Henderson <scott.mc.henderson@gmail.com>
2024-03-10 14:35:53 -0700

    intial plugin

---
+++ PacCentral-Plugin.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * PacCentral Plugin
+ *
+ * @package     PacCentral
+ * @author      Scott Henderson
+ * @copyright   2019 Scott Henderson
+ * @license     GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name: Hello World
+ * Plugin URI:  https://www.paccentraljc.org/
+ * Description: This plugin interfaces with the Orchid Society Pacific Central Judging Center database and the website
+ * Version:     1.0.0
+ * Author:      Scott Henderson
+ * Author URI:  https://scotthenderson.netlify.app/
+ * Text Domain: PacCentral-Plugin
+ * License:     GPL v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
