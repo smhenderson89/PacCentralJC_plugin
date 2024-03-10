8bd5df8b8f8acce4b36919243217cd5ed4517ef7
Scott Henderson <scott.mc.henderson@gmail.com>
2024-03-10 14:41:04 -0700

    hello world plugin for Pac Central Admin page

--- PacCentral-Plugin.php
+++ PacCentral-Plugin.php
@@ -8,7 +8,7 @@
  * @license     GPL-2.0-or-later
  *
  * @wordpress-plugin
- * Plugin Name: Hello World
+ * Plugin Name: PacCentral-Plugin
  * Plugin URI:  https://www.paccentraljc.org/
  * Description: This plugin interfaces with the Orchid Society Pacific Central Judging Center database and the website
  * Version:     1.0.0
@@ -18,3 +18,17 @@
  * License:     GPL v2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
+
+ function display_Pac_Central_page() {
+    echo 'Welcome to Pac Central Admin page';
+  }
+  function Pac_Central_admin_menu() {
+    add_menu_page(
+          'Pac Central Admin',// page title
+          'Pac Central Admin',// menu title
+          'manage_options',// capability
+          'Pac Central Admin',// menu slug
+          'display_Pac_Central_page' // callback function
+      );
+  }
+  add_action('admin_menu', 'Pac_Central_admin_menu');
